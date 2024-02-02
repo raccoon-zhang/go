bba419810c4524e00cd9edfd2e7b48dfb8823731
zhangshaohua <826940531@qq.com>
2024-02-02 19:50:27 +0800

    [Upd] 使用openai框架代码实现gpt问询逻辑
    [遗留问题]
    1.使用了免费的apiKey，该key需要使用另外的url而非官方url，需要改框架内容，可能不太合适，等等看框架能不能更新

--- gptChat/gptChat.go
+++ gptChat/gptChat.go
@@ -1,61 +1,64 @@
 package gptChat
 
 import (
-	"bytes"
-	"encoding/json"
-	"errors"
+	"context"
 	"fmt"
-	"io"
-	"net/http"
 	"os"
 	"strings"
+
+	"github.com/sashabaranov/go-openai"
 )
 
 //url及参数信息详见：https://github.com/bluoruo/go-gtp3.5
 
-type msgBox struct {
-	Role    string `json:"role"` //"user"代表话题内容，"system"代表话题类型
-	Content string `json:"content"`
-}
+// type msgBox struct {
+// 	Role    string `json:"role"` //"user"代表话题内容，"system"代表话题类型
+// 	Content string `json:"content"`
+// }
 
-// POST方法参数列表
-type RequestInfo struct {
-	Messages         []msgBox `json:"messages"`
-	Model            string   `json:"model"`
-	MaxTokens        uint     `json:"max_tokens"`
-	Temperature      float64  `json:"temperature"`
-	TopP             uint     `json:"top_p"`
-	Stream           bool     `json:"stream"`
-	N                uint     `json:"n"`
-	PresencePenalty  float64  `json:"presence_penalty"`
-	FrequencyPenalty float64  `json:"frequency_penalty"`
-	User             string   `json:"user"`
-}
+// // POST方法参数列表
+// type RequestInfo struct {
+// 	Messages         []msgBox `json:"messages"`
+// 	Model            string   `json:"model"`
+// 	MaxTokens        uint     `json:"max_tokens"`
+// 	Temperature      float64  `json:"temperature"`
+// 	TopP             uint     `json:"top_p"`
+// 	Stream           bool     `json:"stream"`
+// 	N                uint     `json:"n"`
+// 	PresencePenalty  float64  `json:"presence_penalty"`
+// 	FrequencyPenalty float64  `json:"frequency_penalty"`
+// 	User             string   `json:"user"`
+// }
 
-type UsageInfo struct {
-	PromptTokens     uint `json:"prompt_tokens"`
-	CompletionTokens uint `json:"completion_tokens"`
-	TotalTokens      uint `json:"total_tokens"`
-}
+// type UsageInfo struct {
+// 	PromptTokens     uint `json:"prompt_tokens"`
+// 	CompletionTokens uint `json:"completion_tokens"`
+// 	TotalTokens      uint `json:"total_tokens"`
+// }
 
-// gpt返回信息
-type responceInfo struct {
-	Id      string    `json:"id"`
-	Object  string    `json:"object"`
-	Created uint      `json:"created"`
-	Usage   UsageInfo `json:"usage"`
-	Choices struct {
-		Msg          msgBox `json:"message"`
-		FinishReason string `json:"finish_reason"`
-		Index        uint   `json:"index"`
-	}
-}
+// // gpt返回信息
+// type responceInfo struct {
+// 	Id      string    `json:"id"`
+// 	Object  string    `json:"object"`
+// 	Created uint      `json:"created"`
+// 	Usage   UsageInfo `json:"usage"`
+// 	Choices struct {
+// 		Msg          msgBox `json:"message"`
+// 		FinishReason string `json:"finish_reason"`
+// 		Index        uint   `json:"index"`
+// 	}
+// }
 
 var gptUrl string
 var apiKey string
+var defaultCompleReq openai.ChatCompletionRequest
 
 func init() {
-	gptUrl = "https://api.openai.com/v1/chat/completions"
+	defaultCompleReq = openai.ChatCompletionRequest{
+		Model:    openai.GPT3Dot5Turbo,
+		Messages: make([]openai.ChatCompletionMessage, 0),
+	}
+	gptUrl = "https://api.chatanywhere.com.cn/v1"
 	content, err := os.ReadFile("../apiKey")
 	if err != nil {
 		fmt.Print("get apiKey err:", err)
@@ -64,55 +67,34 @@ func init() {
 	apiKey = strings.TrimRight(string(content), "\n")
 }
 
-func (defaultReqInfo *RequestInfo) setDefatuReqInfo() {
-	defaultReqInfo.Model = "gpt-3.5-turbo"
-	defaultReqInfo.PresencePenalty = 1.0
-	defaultReqInfo.FrequencyPenalty = 1.0
-	defaultReqInfo.User = "default"
-	var msg = msgBox{
-		Role:    "system",
-		Content: "developer",
-	}
-	defaultReqInfo.Messages = make([]msgBox, 0)
-	defaultReqInfo.Messages = append(defaultReqInfo.Messages, msg)
-}
+// func (defaultReqInfo *RequestInfo) setDefatuReqInfo() {
+// 	defaultReqInfo.Model = "gpt-3.5-turbo"
+// 	defaultReqInfo.PresencePenalty = 1.0
+// 	defaultReqInfo.FrequencyPenalty = 1.0
+// 	defaultReqInfo.User = "default"
+// 	var msg = msgBox{
+// 		Role:    "system",
+// 		Content: "developer",
+// 	}
+// 	defaultReqInfo.Messages = make([]msgBox, 0)
+// 	defaultReqInfo.Messages = append(defaultReqInfo.Messages, msg)
+// }
 
 func QueryGpt(userMsg string) (interface{}, error) {
-	var reqInfo RequestInfo
-	reqInfo.setDefatuReqInfo()
-	reqInfo.Messages = append(reqInfo.Messages, msgBox{
-		Role:    "user",
+	client := openai.NewClient(apiKey)
+	defaultCompleReq.Messages = append(defaultCompleReq.Messages, openai.ChatCompletionMessage{
+		Role:    openai.ChatMessageRoleUser,
 		Content: userMsg,
 	})
-	jsonData, err := json.Marshal(reqInfo)
-	if err != nil {
-		return nil, err
-	}
-	req, err := http.NewRequest("POST", gptUrl, bytes.NewBuffer(jsonData))
-	if err != nil {
-		fmt.Println("create req wrong:", err)
-		return nil, err
-	}
-	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", apiKey))
-	client := &http.Client{}
-	responce, err := client.Do(req)
+	resp, err := client.CreateChatCompletion(
+		context.Background(),
+		defaultCompleReq,
+	)
 
 	if err != nil {
+		fmt.Printf("ChatCompletion error: %v\n", err)
 		return nil, err
-	} else {
-		defer responce.Body.Close()
-		responceBody, err := io.ReadAll(responce.Body)
-		if err != nil || responce.StatusCode != http.StatusOK {
-			fmt.Println("err:", err)
-			fmt.Println("responceBody:", string(responceBody))
-			return nil, errors.New("read responceBody wrong")
-		} else {
-			var responceJsonData responceInfo
-			err := json.Unmarshal(responceBody, &responceJsonData)
-			if err != nil {
-				return nil, err
-			}
-			return responceJsonData.Choices.Msg, nil
-		}
 	}
+
+	return resp.Choices[0].Message.Content, nil
 }
