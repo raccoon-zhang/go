3cc7a05d49559e055b26ec2cc07248fb380a61fd
zhangshaohua <zhangshaohua@itiger.com>
2024-09-25 15:09:52 +0800

    Initial Commit

---
+++ quickPaste/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  quickPaste
+//
+//  Created by zhangshaohua on 2024/9/25.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ quickPasteUITests/quickPasteUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  quickPasteUITestsLaunchTests.swift
+//  quickPasteUITests
+//
+//  Created by zhangshaohua on 2024/9/25.
+//
+
+import XCTest
+
+final class quickPasteUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
